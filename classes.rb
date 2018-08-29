e7d3f431590ade9cf012c388cb49930036535bb0
Ivan Rahn <ivan.rahn@gmail.com>
2018-08-29 13:58:35 +1000

    add timestamp and change hash to array of hashes

--- classes.rb
+++ classes.rb
@@ -2,13 +2,13 @@ require "Geocoder"
 
 class Dog
   attr_accessor :location, :name, :color
-
+#TODO set gettes/setters
   def initialize(name, color, location)
     @name = name
     @color = color
     @location = location
+    @history = []
     @i = 0
-    @history = {}
   end
 
   def speak
@@ -18,31 +18,34 @@ class Dog
   end
 
   def walk(loc, distance)
-    @loc = loc
-    @distance = distance
-
-    if !@history.has_key? @loc
-      @history[@loc] = [@distance, 1]
-    else
-      @history[@loc][0] += @distance
-      @history[@loc][1] += 1
-      results = Geocoder.search(@loc)
-      @history[@loc][2] = results.first.coordinates
-    end
+    @i += 1
+    results = Geocoder.search(loc)
+    coordinates = results.first.coordinates
+    time = Time.now
+    @history << {
+      number: @i,
+      location: loc,
+      distance: distance,
+      time: time,
+      coordinates: coordinates,
+    }
     self
+  
   end
-
+  #check quick
   def display_walk
-    # puts "I have been for #{@i} walks in #{@loc} for #{@distance} meters"
-    @history.each { |key, value|
-      puts "#{key} : #{value}"
+    @history.each { |hash|
+      puts "#{hash[:number]}) Location of the walk: #{hash[:location]}"
+      puts "Distance walked: #{hash[:distance]}"
+      puts "Time: #{hash[:time]}"
+      puts "Coordinates: #{hash[:coordinates]}"
+      puts "------------------------------------------------------------"
     }
   end
 
   def total_distance
-    @sum = 0
-    @history.each_value { |value| @sum += value[0] }
-    puts @sum
+    
+    puts @history.sum { |hash| hash[:distance] }
   end
 end
 
